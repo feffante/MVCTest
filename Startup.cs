967f01a9f9c26483edb56c4258b05666bbf4b8b5
Federico Casali <federico.casali@gmail.com>
2020-03-22 23:20:25 +0100

    inserita prima view

--- Startup.cs
+++ Startup.cs
@@ -22,18 +22,26 @@ namespace UDEMY
             services.AddMvc();
             services.AddMvc(option => option.EnableEndpointRouting = false);
              //MvcOptions.EnableEndpointRouting = false
- 
+            // services.AddLiveReload();
+             services.AddLiveReload(config =>
+             {
+                 config.LiveReloadEnabled = true;
+            // config.FolderToMonitor = Path.GetFullname(PathString.Combine(Env.ContentRootPath,".."));
+             });
+             services.AddRazorPages().AddRazorRuntimeCompilation();
+             services.AddMvc().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+              app.UseLiveReload();
             if (env.IsEnvironment("Development"))
             {
                 app.UseDeveloperExceptionPage();
             }
             app.UseStaticFiles();           
-
+          
             //app.UseMvcWithDefaultRoute();
             app.UseMvc(routeBuilder =>
             {
